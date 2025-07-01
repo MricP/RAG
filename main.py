775d10c07de542b9465da5054a80d0b98c471f97
Pirrera Emric <emric.pirrera@etu.univ-lyon1.fr>
2025-07-01 10:23:42 +0200

    PDFs() -> Fonction de récupération des pdfs

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-import pypdf
-print(pypdf.__version__)
+from langchain_community.document_loaders import PyPDFLoader
+import asyncio
+# "./docs-pdf/c-api.pdf"
+async def loadPdf(filepath : str):
+    loader = PyPDFLoader(filepath)
+    pages = []
+    async for page in loader.alazy_load():
+        pages.append(page)
+    return pages
+
+
+if __name__ == "__main__":
+    pages = asyncio.run(loadPdf("./docs-pdf/c-api.pdf"))
+    for page in pages:
+        print(page.page_content)
